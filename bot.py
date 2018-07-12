bd2bfe95843a9979d40493fcbabbf5ae4334b2f5
fireasembler <ariel.f2002@gmail.com>
2018-07-12 08:43:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import praw
 import os
+import time
 
 reddit = praw.Reddit(client_id = os.environ.get('ID') , client_secret = os.environ.get('SECRET') , user_agent = 'faf' , username = os.environ.get('USERNAME') , password = os.environ.get('PASSWORD'))
 collective = reddit.subreddit('collectivecg')
 
-for post in collective.new(limit = 100):
-    if post.score <= 0:
-        post.upvote()
+while True:
+    for post in collective.new(limit = 100):
+        if post.score <= 0:
+            post.upvote()
+    time.sleep(600)
